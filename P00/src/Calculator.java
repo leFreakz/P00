6d8efde9b421b0b7a069a479543e250c152e426a
21036941 <21036941@DESKTOP-FDVCE26>
2022-05-27 14:09:45 +0800

    First push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21036941, 27 May 2022 11:24:55 am
+ */
+
+public class Calculator {
+
+	public int substract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a - b;
+	}
+	
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
---
+++ P00/src/CalculatorTest.java
@@ -0,0 +1,92 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 21036941, 27 May 2022 11:23:49 am
+ */
+
+public class CalculatorTest {
+
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSubtract() {
+		//fail("Not yet implemented");
+			int a = 100;
+			int b = 2;
+				
+			Calculator cal = new Calculator();
+			int actual = cal.substract (a, b); 
+				 
+			int expected = 98;
+			assertEquals (expected, actual);
+	}
+	
+	@Test
+	public void testAdd() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b); 
+			 
+		int expected = 5555;
+		assertEquals (expected, actual);
+	}
+	
+	@Test
+	public void testMultiple() {
+		int a = 100;
+		int b = 2;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.multiple(a, b); 
+			 
+		int expected = 200;
+		assertEquals (expected, actual);
+		}
+	
+	@Test
+	public void testDivide() {
+		int a = 100;
+		int b = 2;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.divide (a, b); 
+			 
+		int expected = 50;
+		assertEquals (expected, actual);
+		}
+
+
+}
